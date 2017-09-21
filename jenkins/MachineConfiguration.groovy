7ca0dd14dc653fee611a8a1be73e0903fa0e3560
Theofilos Manitaras <teojgor@gmail.com>
2017-09-21 08:10:18 +0200

    Add functions in separate file

--- jenkins/MachineConfiguration.groovy
+++ jenkins/MachineConfiguration.groovy
@@ -13,3 +13,37 @@ enum MachineConfiguration {
         return configuration
     }
 }
+
+MachineConfiguration getMachineConfiguration(String message, String machine) {
+    def machinePattern = ".*${machine}.*"
+    def machineGPU = ".*${machine}-gpu.*"
+    def machineMC = ".*${machine}-mc.*"
+
+    if (message ==~ machineGPU) {
+        if (message ==~ machineMC) {
+            return config.MachineConfiguration.BOTH
+        }
+        else {
+            return config.MachineConfiguration.GPU
+        }
+    }
+    else if (message ==~ machineMC) {
+        return MachineConfiguration.MC
+    }
+     
+    return config.MachineConfiguration.BOTH
+}
+
+boolean machineCheck(String message, String machine) {
+    def machinePattern = ".*${machine}.*"
+    
+    return message ==~ machinePattern? true : false 
+}
+
+boolean checkWorkInProgress(String message) {
+    if (message ==~ /.*WIP.*/) {
+        return true
+    }
+    
+    return false
+}
