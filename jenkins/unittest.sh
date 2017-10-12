ec2eadf8a00230aa6090fb6a835a689ab3700f3a
Theofilos Manitaras <manitaras@cscs.ch>
2017-10-12 08:04:36 +0200

    Check if clause

--- jenkins/unittest.sh
+++ jenkins/unittest.sh
@@ -4,32 +4,25 @@ EASYBUILD_TMPDIR=${PREFIX}/tmp
 EASYBUILD_SOURCE_PATH=${PREFIX}/sources 
 status=0
 
-if [ -d $PREFIX ]; then
-    rm -rf $PREFIX/*
-else 
-    mkdir $PREFIX
-fi
+#if [ -d $PREFIX ]; then
+#    rm -rf $PREFIX/*
+#else 
+#    mkdir $PREFIX
+#fi
 
-offlist="a/Amber c/CPMD n/NAMD n/NCL u/UDUNITS v/VASP v/Visit"
-pushd $HOME
-for item in ${offlist}; do 
-    cp --parents -r sources/$item $PREFIX
-done
-popd 
+#offlist="a/Amber c/CPMD n/NAMD n/NCL u/UDUNITS v/VASP v/Visit"
+#pushd $HOME
+#for item in ${offlist}; do 
+#    cp --parents -r sources/$item $PREFIX
+#done
+#popd 
       
-# --- BUILD ---
-if [[ "$system" =~ "daint" || "$system" =~ "dom" ]]; then
-    for ARCH in ${arch_list}; do
-        linkname="${system}-${ARCH}"
-        ${command/ARCH/$ARCH} $PWD/jenkins-builds/production.sh --arch=$ARCH --list=$PWD/jenkins-builds/${linkname} --prefix=${PREFIX}/${ARCH} --unuse=${unuse_path/ARCH/$ARCH} --xalt=no
-        status=$[status+$?]
-        echo $linkname
-    done
+if [[ $ACRCH=="" ]]; then
+    #$command $PWD/jenkins-builds/production.sh --list=$PWD/jenkins-builds/${linkname} --prefix=$PREFIX --unuse=${unuse_path}
+    echo "Hello from first if branch" 
 else
-    linkname=${system}
-    $command $PWD/jenkins-builds/production.sh --list=$PWD/jenkins-builds/${linkname} --prefix=$PREFIX --unuse=${unuse_path}
-    status=$[status+$?]
-    echo $linkname
+#    ${command/ARCH/$ARCH} $PWD/jenkins-builds/production.sh --arch=$ARCH --list=$PWD/jenkins-builds/${linkname} --prefix=${PREFIX}/${ARCH} --unuse=${unuse_path/ARCH/$ARCH} --xalt=no
+    echo "Hello from second if branch"
 fi 
+status=$[status+$?]
 exit ${status}
-
